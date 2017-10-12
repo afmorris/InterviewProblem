08ba1f8f450f82c26d03f71e3e783a618626c771
Tony Morris <afmorris@users.noreply.github.com>
2017-10-12 12:54:35 -0400

    Update MergeArray.cs
    Just a little overhead with the usage of the `Queue<T>` object, but it was simpler than working directly with the input arrays. Should be 
    a O(4n+4m) operation, where `n` is the number of items in `array1` and `m` is the number of items in `array2`. This is, by no means, the most performant solution.
    
    Some individual performance notes:
    * Instantiating the `Queue` with a collection is O(n) where n is the number of items in the input collection.
    * Instantiating the `Queue` with a capacity is O(n) where n is the capacity.
    * Calling `Queue.Count` is O(1)
    * Calling `Queue.Enqueue` in which the current Count is less than than the capacity (because we instantiated it with a capacity) is O(1). Otherwise it's O(n) because it has to resize.
    * Calling `Queue.Dequeue` is O(1)
    * Calling `Queue.ToArray` is O(n) where n is the current Queue's Count

--- InterviewProblem/InterviewProblem/MergeArray.cs
+++ InterviewProblem/InterviewProblem/MergeArray.cs
@@ -1,8 +1,4 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace InterviewProblem
 {
@@ -10,7 +6,37 @@ namespace InterviewProblem
     {
         public static char[] Merge(char[] array1, char[] array2)
         {
-            throw new NotImplementedException();
+            if (array1 == null || array1.Length == 0)
+            {
+                return array2;
+            }
+
+            if (array2 == null || array2.Length == 0)
+            {
+                return array1;
+            }
+
+            var array1Queue = new Queue<char>(array1);
+            var array2Queue = new Queue<char>(array2);
+            
+            var output = new Queue<char>(array1.Length + array2.Length);
+
+            while (array1Queue.Count > 0)
+            {
+                output.Enqueue(array1Queue.Dequeue());
+
+                if (array2Queue.Count > 0)
+                {
+                    output.Enqueue(array2Queue.Dequeue());
+                }
+            }
+
+            while (array2Queue.Count > 0)
+            {
+                output.Enqueue(array2Queue.Dequeue());
+            }
+
+            return output.ToArray();
         }
     }
 }
